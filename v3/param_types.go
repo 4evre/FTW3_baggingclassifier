107b52dcf805e33363f9e5c55b8a0a8ded673a0c
4evre <105898283+4evre@users.noreply.github.com>
2022-06-02 11:25:47 +0000

    Refactor layout

--- v3/param_types.go
+++ v3/param_types.go
@@ -4,3 +4,6 @@ package v3
 type GetCoinMarketsParams struct {
 	VSCurrency            string   `q:"vs_currency" required:"true"`
 	IDs                   []string `q:"ids"`
+	Category              string   `q:"category"`
+	Order                 string   `q:"order"`
+	PerPage               uint16   `q:"per_page"`
