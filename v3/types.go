49154a9e2295e3a933ded7439752165bbbdcd494
4evre <105898283+4evre@users.noreply.github.com>
2022-07-08 22:39:15 +0000

    vlan: fix a race in egress prio management

--- v3/types.go
+++ v3/types.go
@@ -11,3 +11,5 @@ type Coin struct {
 	ID                           string                  `json:"id"`
 	Symbol                       string                  `json:"symbol"`
 	Name                         string                  `json:"name"`
+	AssetPlatformID              string                  `json:"asset_platform_id"`
+	BlockTimeInMinutes           uint16                  `json:"block_time_in_minutes"`
