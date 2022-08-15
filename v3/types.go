c19a9733a1ecc14b3207145fbe26da88dc6b2d6f
4evre <105898283+4evre@users.noreply.github.com>
2022-08-15 16:07:57 +0000

    Fixes #46

--- v3/types.go
+++ v3/types.go
@@ -89,3 +89,4 @@ type CoinMarketData struct {
 	PriceChangePercentage1y                float64            `json:"price_change_percentage_1y"`
 	MarketCapChange24h                     float64            `json:"market_cap_change_24h"`
 	MarketCapChangePercentage24h           float64            `json:"market_cap_change_percentage_24h"`
+	PriceChange24hInCurrency               map[string]float64 `json:"price_change_24h_in_currency"`
