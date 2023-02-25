68388f60dbdd31320fc25574524c829c37038df7
4evre <105898283+4evre@users.noreply.github.com>
2023-02-25 11:31:31 +0000

    Removed wrong comment

--- v3/v3.go
+++ v3/v3.go
@@ -40,3 +40,4 @@ func GetCoinMarketChart(id string, params GetCoinMarketChartParams) (CoinMarketC
 func GetCoinMarketChartRange(id string, params GetCoinMarketChartRangeParams) (CoinMarketChart, error) {
 	var marketChart CoinMarketChart
 	err := getWithParams("/coins/"+id+"/market_chart/range", params, &marketChart)
+	return marketChart, err
